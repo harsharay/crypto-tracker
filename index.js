3ebc9b8c702ded3a08fe2944d32d9450fdb60a96
harsharay <harsharay.ray@gmail.com>
2021-04-03 02:47:10 +0530

    Added logbox ingore all logs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name} from './app.json';
+import { LogBox } from "react-native"
+
+LogBox.ignoreAllLogs(true)
 
 AppRegistry.registerComponent(name, () => App);
